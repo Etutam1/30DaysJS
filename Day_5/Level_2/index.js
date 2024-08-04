a11df547076e37aae725d8b74a185d826bddb4ce
Etutam1 <matute1172@gmail.com>
2024-08-04 19:38:57 +0200

    finished day5 exercices

--- Day_5/Level_2/index.js
+++ Day_5/Level_2/index.js
@@ -1,6 +1,6 @@
 //1. Create a separate countries.js file and store the countries array in to this file, create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file
-const countries = require("./countries");
-const techs = require("./web_techs");
+const countries = require("../countries");
+const techs = require("../web_techs");
 
 // 2.First remove all the punctuations and change the string to array and count the number of words in the array
 
--- Day_5/Level_3/index.js
+++ Day_5/Level_3/index.js
@@ -1,3 +1,5 @@
+const countries = require("../countries");
+const techs = require("../web_techs");
 // 1.The following is an array of 10 students ages: const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 // Sort the array and find the min and max age
 // Find the median age(one middle item or two middle items divided by two)
@@ -34,18 +36,70 @@ function e1() {
   const avg = sum / ages.length;
   console.log(`The avg. age is ${avg}`);
   //   range = ages.pop() - ages.shift();
-  let minDiff = min - avg
-  let maxDiff = max - avg
+  let minDiff = min - avg;
+  let maxDiff = max - avg;
   // console.log(`The range of ages is ${range}`)
 
   if (Math.abs(minDiff) > Math.abs(maxDiff)) {
-    console.log(`Min difference (${minDiff.toFixed(2)}) is greater than max difference (${maxDiff.toFixed(2)})`);
-} else if (Math.abs(minDiff) < Math.abs(maxDiff)) {
-    console.log(`Max difference (${maxDiff.toFixed(2)}) is greater than min difference (${minDiff.toFixed(2)})`);
-} else {
-    console.log(`Min difference (${minDiff.toFixed(2)}) is equal to max difference (${maxDiff.toFixed(2)})`);
-}
-
+    console.log(
+      `Min difference (${minDiff.toFixed(
+        2
+      )}) is greater than max difference (${maxDiff.toFixed(2)})`
+    );
+  } else if (Math.abs(minDiff) < Math.abs(maxDiff)) {
+    console.log(
+      `Max difference (${maxDiff.toFixed(
+        2
+      )}) is greater than min difference (${minDiff.toFixed(2)})`
+    );
+  } else {
+    console.log(
+      `Min difference (${minDiff.toFixed(
+        2
+      )}) is equal to max difference (${maxDiff.toFixed(2)})`
+    );
+  }
 }
 // e1();
-//TODO Level 3  last 3 exer. about countries array
+// 2.Slice the first ten countries from the countries array
+function e2() {
+  console.log("Contries: ", countries.toString());
+  countries.splice(0, 10);
+  console.log("Contries after slice: ", countries.toString());
+}
+// e2();
+
+// 3.Find the middle country(ies) in the countries array
+
+function e3() {
+  let middle;
+  let result;
+  if (countries.length % 2 == 0) {
+    middle = countries.length / 2 - 1;
+    result = countries.slice(middle, middle + 2);
+  } else {
+    middle = Math.floor(countries.length / 2);
+    result = countries.slice(middle, middle + 1);
+  }
+
+  console.log(`The middle in countries: ${result}`);
+}
+// e3();
+
+// 4.Divide the countries array into two equal arrays if it is even. If countries array is not even , one more country for the first half.
+function e4() {
+  
+  console.log('Countries before split: ', countries.toString())
+  if (countries.length % 2 == 0) {
+    countries[1] = countries.slice(0,countries.length/2)
+    countries[0] = countries.slice(countries.length/2, -1)
+  } else {
+    countries[1] = countries.slice(0,countries.length/2+1)
+    countries[0] = countries.slice(countries.length/2+1, -1)
+    
+  }
+  countries.reverse()
+  countries.splice(0,9)
+  console.log('Countries after sliced: ', countries)
+}
+// e4()
