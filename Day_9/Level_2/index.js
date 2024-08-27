deb519c0074d7f10eaf9661105849c69e5b8b2ba
Etutam1 <matute1172@gmail.com>
2024-08-27 20:56:07 +0200

    level 2finished

--- Day_9/Level_2/index.js
+++ Day_9/Level_2/index.js
@@ -60,4 +60,43 @@ function countriesLetters(){
     
 }
 
-countriesLetters()
+// countriesLetters()
+
+// 5.Declare a getFirstTenCountries function and return an array of ten countries. 
+// Use different functional programming to work on the countries.js array
+
+function getFirstTenCountries(arr){
+
+    return arr.filter((el,i)=>
+        i < 10
+    )
+}
+
+// console.log(getFirstTenCountries(countries));
+
+// 6.Declare a getLastTenCountries function which which returns the last ten countries in the countries array.
+
+function getLastTenCountries(arr){
+    return arr.filter((c,i)=> i >= arr.length - 10)
+}
+
+// console.log(getLastTenCountries(countries));
+
+
+// 7.Find out which letter is used many times as 
+// initial for a country name from the countries array (eg. Finland, Fiji, France etc)
+
+function mostUsedInitial(arr){
+        let firstLetter = ''
+   return Object.values(arr.reduce((acc,c)=>{
+        firstLetter = c.name.charAt(0)
+        if(!acc[firstLetter]){
+            acc[firstLetter] = {letter : firstLetter, times: 1};
+        }else{
+            acc[firstLetter].times += 1;
+        }
+        return acc
+    },{})).sort((a,b)=> b.times - a.times)[0]
+}
+
+// console.log(`The most used letter in the array is the letter ${mostUsedInitial(countries).letter}`)
