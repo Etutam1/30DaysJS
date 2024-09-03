cfdd6b4efdb3687ba9e5fdc52bd7f4e6bdd7d20c
Etutam1 <matute1172@gmail.com>
2024-09-03 20:57:15 +0200

    finished day 10

---
+++ Day_10/Level_3/index.js
@@ -0,0 +1,28 @@
+require("module-alias/register");
+const countries = require("@arrays/countries_data");
+
+// 1.How many languages are there in the countries object file.
+
+let langs = 
+  countries.reduce((acc, c) => {
+    c.languages.forEach((l) => {
+      acc.push(l);
+    });
+    return acc;
+  }, [])
+
+console.log("langs count:", new Set(langs).size);
+
+// 2.*** Use the countries data to find the 10 most spoken languages:
+
+const mostSpokenLang = function (limit) {
+
+  let count = []
+  for (const lang of new Set(langs)) {
+    count.push({lang:lang, count:langs.filter(l=>l == lang).length})
+  }
+
+  return count.sort((a,b)=> b.count - a.count).filter((c,i)=> i < limit)
+}
+
+console.log(mostSpokenLang(10))
